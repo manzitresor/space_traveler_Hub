28971611e27c94f9f544964ca39470b10fbe6314
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-07-24 16:36:58 +0100

    Add missions page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './pages/Layout';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
+import Missions from './pages/Missions';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="profile" element={<Profile />} />
+          <Route path="missions" element={<Missions />} />
         </Route>
       </Routes>
     </BrowserRouter>
