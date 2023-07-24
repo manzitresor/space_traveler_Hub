6bfae28e48d8bebd3ceefa72289b1542527acfc7
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-07-24 16:05:32 +0200

    add Project setup: create empty navlink

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import NavLink from './components/NavLink';
+
 function App() {
   return (
     <div>
       <h1>Hello!</h1>
+      <NavLink />
     </div>
   );
 }
---
+++ src/components/NavLink.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NavLink() {
+  return (
+    <div>
+      <h1>NavLink</h1>
+
+    </div>
+  );
+}
+
+export default NavLink;
