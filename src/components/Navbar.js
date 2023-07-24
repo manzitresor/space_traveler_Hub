df1c33c6f6f59dc0493c98eb1643b56b7f6a2ab3
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-07-24 16:06:14 +0100

    Add active underline feature

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,8 +7,8 @@ function Navbar() {
     <nav>
       <img className="nav-logo" src={logo} width={40} alt="Navbar logo" />
       <ul className="nav-items">
-        <li className="nav-item-list"><NavLink to="/" activeClassName="active">Home</NavLink></li>
-        <li className="nav-item-list"><NavLink to="/profile" activeClassName="active">Profile</NavLink></li>
+        <li className="nav-item-list"><NavLink to="/" activeClassName="active"><p className="tab">Home</p></NavLink></li>
+        <li className="nav-item-list"><NavLink to="/profile" activeClassName="active"><p className="tab">Profile</p></NavLink></li>
       </ul>
     </nav>
   );
