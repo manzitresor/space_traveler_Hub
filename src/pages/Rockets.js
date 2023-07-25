bc8dbb17f2a8e4104d92fe00aa0bbe25c65624df
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-07-25 15:33:38 +0200

    Add fetched data from rockets API

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchRockets } from '../redux/rockets/rocketsSlice';
 
 function Rockets() {
+  const { data, error, isloading } = useSelector((state) => state.rockets);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
+  if (isloading === true) {
+    return (
+      <div>
+        loading....
+      </div>
+    );
+  }
+  if (error) {
+    return (
+      <div>
+        {error}
+      </div>
+    );
+  }
+  if (data) {
+    console.log(data);
+  }
   return (
     <div>Rockets</div>
   );
---
+++ src/redux/rockets/rocketsSlice.js
@@ -0,0 +1,47 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const url = 'https://api.spacexdata.com/v3/rockets';
+
+export const fetchRockets = createAsyncThunk('rockets/fetchrockets', async (thunkApi) => {
+  try {
+    const res = await axios.get(url);
+    return res.data;
+  } catch (error) {
+    return thunkApi.rejectWithValue('ooops something wrent wrong');
+  }
+});
+const initialState = {
+  data: [],
+  isloading: false,
+  error: false,
+};
+
+const rocketsSlice = createSlice({
+  name: 'rockets',
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(fetchRockets.pending, (state) => {
+      state.isloading = true;
+    });
+    builder.addCase(fetchRockets.fulfilled, (state, action) => {
+      state.isloading = false;
+      const fetchdata = action.payload.map((item) => {
+        const rockets = {};
+        rockets.id = item.id;
+        rockets.name = item.rocket_name;
+        rockets.type = item.rocket_type;
+        rockets.image = item.flickr_images;
+        return rockets;
+      });
+      state.data = fetchdata;
+    });
+    builder.addCase(fetchRockets.rejected, (state, action) => {
+      state.isloading = false;
+      state.error = action.payload;
+    });
+  },
+});
+
+export default rocketsSlice.reducer;
