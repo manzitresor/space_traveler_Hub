f64947b1914ae8c0c2fea47a6c9f19f9eb4a1965
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-07-26 08:48:30 +0200

    Add rocket booking and cancel booking

--- src/pages/Rocket.js
+++ src/pages/Rocket.js
@@ -1,9 +1,22 @@
+/* eslint-disable max-len */
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
+import Badge from 'react-bootstrap/Badge';
+import { useDispatch, useSelector } from 'react-redux';
+import { booking, cancelBooking } from '../redux/rockets/rocketsSlice';
 
 function Rocket({ item }) {
+  const dispatch = useDispatch();
+  const { reserved } = useSelector((state) => state.rockets.data.find((rocket) => rocket.id === item.id));
+  const handleReserve = (id) => {
+    dispatch(booking(id));
+  };
+
+  const handleCancelReserve = (id) => {
+    dispatch(cancelBooking(id));
+  };
   return (
     <>
       <div className="rocket-container">
@@ -11,8 +24,21 @@ function Rocket({ item }) {
           <img src={item.image} alt="rocket" />
           <div className="rocket-card-content">
             <h1>{item.name}</h1>
+            { reserved ? (
+              <span>
+                {' '}
+                <Badge bg="secondary">reserved</Badge>
+              </span>
+            ) : null }
             <p>{item.description}</p>
-            <Button type="submit">Reserve Rocket</Button>
+            {
+                !reserved ? (<Button type="submit" onClick={() => handleReserve(item.id)}>Reserve Rocket</Button>)
+                  : (
+                    <Button type="submit" onClick={() => handleCancelReserve(item.id)}> Cancel Reservation</Button>
+                  )
+
+            }
+
           </div>
         </div>
       </div>
@@ -27,5 +53,6 @@ Rocket.propTypes = {
     name: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
   }).isRequired,
 };
